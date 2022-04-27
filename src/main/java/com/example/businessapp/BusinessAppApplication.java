710fbe06588d06f167c11891ce499e441b55c760
thuongh2 <vo.thuong01632985@gmail.com>
2022-04-27 21:24:02 +0700

    change encoder

--- src/main/java/com/example/businessapp/BusinessAppApplication.java
+++ src/main/java/com/example/businessapp/BusinessAppApplication.java
@@ -2,15 +2,20 @@ package com.example.businessapp;
 
 import com.example.businessapp.entity.User;
 import com.example.businessapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
 @SpringBootApplication
 public class BusinessAppApplication {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(BusinessAppApplication.class, args);
@@ -19,7 +24,7 @@ public class BusinessAppApplication {
     @Bean
     CommandLineRunner run(UserService userService){
         return args -> {
-           userService.saveUser(new User(1L, "admin", "admin", "admin", "admin@gmail.com", "SUPERADMIN", new ArrayList<>(), true));
+           userService.saveUser(new User(1L, "admin", "admin", passwordEncoder.encode("admin"), , "admin@gmail.com", "SUPERADMIN", new ArrayList<>(), true));
         };
     }
 }
